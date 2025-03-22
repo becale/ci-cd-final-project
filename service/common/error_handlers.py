83634271f79cf682391d331817a8ecf4bc5f1d60
becale <bricetatchou68@gmail.com>
2025-03-22 04:54:17 -0400

    Nex

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -45,9 +45,8 @@ def not_found(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_404_NOT_FOUND, 
-            error="Not Found", message=message)
-        ,
+            status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message
+            ),
         status.HTTP_404_NOT_FOUND,
     )
 
